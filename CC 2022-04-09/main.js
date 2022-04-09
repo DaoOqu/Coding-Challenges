618c16b9e5c7c3c3aa59f765b020e4a617b75281
David Oquendo <davidoqu23@gmail.com>
2022-04-09 11:57:07 -0500

    Solved coding challenge for 04/09/2022

--- CC 2022-04-09/main.js
+++ CC 2022-04-09/main.js
@@ -1,1 +1,13 @@
-//Coding challenge 04/09/2022
+// Coding challenge 04/09/2022
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case. In this case, all triangles must have surface greater than 0 to be accepted
+
+
+function isTriangle(a,b,c) {
+  //checking for all triangles, not just right triangles so pythagorean theorem is just one case
+
+  return (a+b > c && b+c > a && c+a > b);
+}
+
+isTriangle(1,2,2); //true
+isTriangle(7,2,2); //false

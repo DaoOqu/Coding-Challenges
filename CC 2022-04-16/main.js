6135972ea0956833f7ae537255cb511b891721e5
David Oquendo <davidoqu23@gmail.com>
2022-04-16 22:40:05 -0500

    Added alt solution to coding challenge for 04/16/2022

--- CC 2022-04-16/main.js
+++ CC 2022-04-16/main.js
@@ -14,6 +14,12 @@ function minMax (arr) {
   return newArr;
 }
 
+// or
+
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
 minMax([1,2,3,4,5]) // [1,5]
 minMax([2334454, 5]) // [5, 2334454]
 minMax([1]) // [1]

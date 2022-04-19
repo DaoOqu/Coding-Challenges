c106aebc8b69aa7a0df93eb4ea4cf88db4815ba0
David Oquendo <davidoqu23@gmail.com>
2022-04-18 20:44:51 -0500

    Solved coding challenge for 04/18/2022

---
+++ CC 2022-04-18/main.js
@@ -0,0 +1,26 @@
+// Coding challenge 04/17/2022
+
+// make a function which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+// If begin value is greater than the end, function should returns 0
+
+// ex: 
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+
+function sequenceSum(begin, end, step) {
+  let sum = 0
+  for (let i = begin; i <= end; i += step) {
+    sum += i
+  }
+  return sum
+}
+
+sequenceSum(2,2,2) // 2
+sequenceSum(2,6,2) // 12
+sequenceSum(1,5,1) // 15
+sequenceSum(1,5,3) // 5

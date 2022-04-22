b4f443184369fd11095aa09249fdd87a51b77137
David Oquendo <davidoqu23@gmail.com>
2022-04-22 18:19:59 -0500

    Added alt solution for coding challenge 04/22/2022

--- CC 2022-04-22/main.js
+++ CC 2022-04-22/main.js
@@ -16,6 +16,19 @@ function likes(names) {
   if (names.length > 3) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
 
+// or
+
+function likes(names) {
+  names = names || [];
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+}
+
 likes([]) // 'no one likes this'
 likes(['Peter']) // 'Peter likes this'
 likes(['Jacob', 'Alex']) // 'Jacob and Alex like this'

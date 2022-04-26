0322ac505d747fd5144b76ec5e882f7bb1742ee4
David Oquendo <davidoqu23@gmail.com>
2022-04-26 18:06:18 -0500

    Solved coding challenge for 04/26/2022

--- CC 2022-04-26/main.js
+++ CC 2022-04-26/main.js
@@ -1,1 +1,23 @@
 // Coding challenge 04/26/2022
+
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed. Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+const spinWords = string => {
+  let strArr = string.split(' ');
+  let result = '';
+  strArr.map((el, i) => {
+    if (el.length >= 5) {
+      strArr[i] = el.split('').reverse().join('');
+    }else {
+      strArr[i] = el;
+    }
+    result = strArr.join(' ');
+  });
+  return result;
+}
+
+spinWords( "Hey fellow warriors" ) // "Hey wollef sroirraw"
+spinWords( "This is a test") // "This is a test"
+spinWords( "This is another test" ) // "This is rehtona test"

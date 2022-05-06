e71760ad23b79e2198a1f2c334b5e6e895315658
David Oquendo <davidoqu23@gmail.com>
2022-05-06 12:15:00 -0500

    Solved coding challenge for 05/06/2022

--- CC 2022-05-06/main.js
+++ CC 2022-05-06/main.js
@@ -12,3 +12,37 @@
 // Output: 3
 // Explanation: The answer is "wke", with the length of 3.
 // Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+
+// Dynamic programming question??
+
+// long subSTRING not subSEQUENCE meaning must be in line in the original string
+// pwwkew -> substring: wke, subsequence: pwke
+
+// one of each character per substring
+
+
+const lengthOfLongestSubstring = (s) => {
+  if (s.length === 0) return 0;
+  if (s.length === 1) return 1;
+  
+  const map = new Map(); //hashnmap
+  let i = 0; // pointer 1
+  let j = 0; // pointer 2
+  let max = 0; // the value or length we are going to return 
+  
+  while (i < s.length) {
+      while (map.has(s[i])) { // check if char is in hashmap
+          map.delete(s[j]); // if yes, delete it (meaning it will delete all extra/repeating characters)
+          j++ 
+      }
+      map.set(s[i]); // if not, add it 
+      max = Math.max(max, i - j + 1); // calculate the max substring from set (i - j + 1)
+  i++; 
+  }
+  return max; // return max values which would have the substring
+};
+
+// time complexity: O(n^2)
+// space complexity: O()

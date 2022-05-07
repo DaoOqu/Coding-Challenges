952ff48becf8947b88b13bede99360b58046a2a7
David Oquendo <davidoqu23@gmail.com>
2022-05-07 12:12:00 -0500

    Added coding challenge for 05/07/2022

---
+++ CC 2022-05-07/main.js
@@ -0,0 +1,12 @@
+// Coding challenge 05/07/2022
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+

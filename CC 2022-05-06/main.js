e45aa63ae25f4a228a8f84a52b3990ddc9ebb81d
David Oquendo <davidoqu23@gmail.com>
2022-05-08 21:30:54 -0500

    Added coding challenge for 05/06/2022

---
+++ CC 2022-05-06/main.js
@@ -0,0 +1,14 @@
+// Coding challenge 05/06/2022
+
+// Given a string s, find the length of the longest substring without repeating characters.
+
+
+// ex:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+//Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.

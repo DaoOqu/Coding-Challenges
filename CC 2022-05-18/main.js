f03fe63487f52c61671b1352f63bc3bf34ecb0f8
David Oquendo <davidoqu23@gmail.com>
2022-05-18 18:03:34 -0500

    Solved coding challenge for 05/18/2022

--- CC 2022-05-18/main.js
+++ CC 2022-05-18/main.js
@@ -1,1 +1,39 @@
 // Coding challenge 05/18/2022
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Input: head = [1,2]
+// Output: [2,1]
+
+
+
+// [1 -> 2 -> 3 -> 4 -> 5 -> null]
+// [null <- 1 <- 2 <- 3 <- 4 <- 5]
+
+// [5 -> 4 -> 3 -> 2 -> 1 -> null]
+
+
+// two pointer system
+var reverseList = function(head) {
+  // initialize prev and curr
+  let prevNode = null;
+  let currNode = head; // 1
+  
+  while(currNode) {
+      // reference next node
+      let nextNode = currNode.next; // 4
+      // point backwards to the prev node
+      currNode.next = prevNode; // 2 <- 3
+      // reference the next node in prev node
+      prevNode = currNode; // 3
+      // reference the next node in curr node
+      currNode = nextNode; // 4
+  }
+  return prevNode;
+};
+
+// time complexity: O(n)
+// space complexity: O(1)

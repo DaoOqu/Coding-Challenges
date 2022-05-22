91f2fcb89ae38f268edacb91a68bc1f7543bba75
David Oquendo <davidoqu23@gmail.com>
2022-05-21 19:54:17 -0500

    Added coding challenge for 05-21-2022

---
+++ CC 2022-05-21/main.js
@@ -0,0 +1,10 @@
+// Coding challenge 05/21/2022
+
+// Given the num parameter, return the string true if the parameter is a prime Number, otherwise return the string false. The range will be between 1 and 2^16 
+
+// ex:
+// input: 19
+// output: true
+
+// input: 110
+// output: false

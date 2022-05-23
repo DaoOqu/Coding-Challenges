ec8097ac8dc818689df68bf7fd1ef66ad8fbc771
David Oquendo <davidoqu23@gmail.com>
2022-05-22 20:00:28 -0500

    Solved coding challenge for 05-21-2022

--- CC 2022-05-21/main.js
+++ CC 2022-05-21/main.js
@@ -8,3 +8,22 @@
 
 // input: 110
 // output: false
+
+// prime number is that which is greater than 1 that is not a product of two smaller natural numbers
+// only has two factors -> itself and 1
+
+// ex: 5 -> prime 
+// ex: 6 -> not prime
+
+// so if a number is divisible by any number larger than 1 it is prime
+
+// check square root
+// if we can't find any factors less than or equal to the square root of num, then num must be prime
+
+function primeTime(num) {
+  let squareRoot = Math.sqrt(num);
+  for(let i = 2; i <= squareRoot; i++) {
+    if(num % i === 0) return false;
+  }
+  return num > 1;
+}

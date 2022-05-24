fcfb605f416d0177a4833289ab2ceb81ad9a9ddb
David Oquendo <davidoqu23@gmail.com>
2022-05-23 20:09:39 -0500

    Added coding challenge for 05/23/2022

---
+++ CC 2022-05-23/main.js
@@ -0,0 +1,10 @@
+// Coding challenge 05/23/2022
+
+// Given the parameter strArr, which will contain 2 strings representing two comma seperated lists of keypresses, return true if the keypresses produce the same printable string and false if they do not.
+// A keypress can be either a printable character or a backspace represented by '-B'. You can produce a printable string from such a string of keypresses by having backspaces erase one preceding character
+
+// ex: ["a,a,a,-B,c", "a,c"]
+// output: false
+
+// ex: ["a,b,c,d,d,-B", "a,b,c,d"]
+// output: true

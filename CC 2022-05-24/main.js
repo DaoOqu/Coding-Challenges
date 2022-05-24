52a0901b211a8e42c3fa68f14973138aec2d3c91
David Oquendo <davidoqu23@gmail.com>
2022-05-24 18:33:24 -0500

    Added solution for 05/24/2022

---
+++ CC 2022-05-24/main.js
@@ -0,0 +1,17 @@
+// Coding challenge 05/24/2022
+
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+
+// Input: nums = [1], k = 1
+// Output: [1]
+
+// will always have at least one element?
+// will they always be integers?
+// are we returning the integer that comes up a 'k' number of times? or
+// are we returning the 'k' most elements that come up the most frequently?
+// will we see integers that come up the same amount of times? and will we have to choose one over the other?
+
+

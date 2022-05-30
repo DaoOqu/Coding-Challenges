49b4158b03703634f6d7866859fc44dd5b026b42
David Oquendo <davidoqu23@gmail.com>
2022-05-29 23:19:53 -0500

    Added coding challenge for 05/29/2022

---
+++ CC 2022-05-29/main.js
@@ -0,0 +1,25 @@
+// Coding challenge 05/29/2022
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+
+

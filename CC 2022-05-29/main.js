727cf9a8fe5bd02cfb65baf70c6cd8d1f1618e88
David Oquendo <davidoqu23@gmail.com>
2022-05-29 23:20:21 -0500

    Solved coding challenge for 05/29/2022

--- CC 2022-05-29/main.js
+++ CC 2022-05-29/main.js
@@ -23,3 +23,40 @@
 
 
 
+// are both lists validated? integers? Whole? Can they be neg?
+// are the lists sorted? in what order?
+// can they be empty? one? or both? same length?
+// up to how many nodes can be in a list?
+
+var mergeTwoLists = function(list1, list2) {
+  
+  // iterate through lists and compare each node
+  // whichever node is smaller, add it to merged list then move to next node
+  // if equal, choose one and move to next node until null is reached
+  // for the case of unequal lists lengths, find the list with remaining nodes ->
+  // and add the remaining nodes to the end of the merged list
+  
+  
+  let dummy = new ListNode();
+  let tail = dummy;
+  
+  while(list1 && list2) {
+    if(list1.val < list2.val) {
+      tail.next = list1;
+      list1 = list1.next;
+    } else {
+      tail.next = list2;
+      list2 = list2.next;
+    }
+    tail = tail.next;
+  }
+  
+  if(list1) {
+    tail.next = list1;
+  } else {
+    tail.next = list2;
+  }
+  
+  return dummy.next;
+  
+};

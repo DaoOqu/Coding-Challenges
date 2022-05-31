29954c30900f88026e199635d912e88dd782671e
David Oquendo <davidoqu23@gmail.com>
2022-05-30 21:35:19 -0500

    Added coding challenge for 05/30/2022

---
+++ CC 2022-05-30/main.js
@@ -0,0 +1,6 @@
+// Coding challenge 05/30/2022
+
+// Write two functions that finds the factorial of any number. 
+// One should use recursive, the other should just use a for loop
+
+// 5! -> 5*4*3*2*1 = 120

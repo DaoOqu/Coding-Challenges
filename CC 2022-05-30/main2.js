846a03161bd7c5586617f10ff9a37506703e4cbd
David Oquendo <davidoqu23@gmail.com>
2022-05-30 22:05:09 -0500

    Solved 2nd coding challenge for 05/30/2022

--- CC 2022-05-30/main2.js
+++ CC 2022-05-30/main2.js
@@ -5,3 +5,29 @@
 // 0,1,1,2,3,5,8,13,21,34,55,89,144 ...
 // the pattern of the sequence is that each value is the sum of the 2 previous values
 // ex: num = 5 -> 2 + 3
+
+
+// Iterative
+
+function fibonacciIterative(num) {
+  let arr = [0, 1];
+
+  for(let i = 2; i < num + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  
+  return arr[num];
+}
+
+
+
+// Recursive
+
+function fibonacciRecursive(num) {
+  if(num < 2) return num;
+
+  return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+}
+
+console.log(fibonacciIterative(2));
+

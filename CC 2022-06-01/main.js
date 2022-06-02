5283eaca039c29fd7c7264f2acbcf65c60b92487
David Oquendo <davidoqu23@gmail.com>
2022-06-01 21:25:36 -0500

    Solved coding challenge for 06/01/2022

---
+++ CC 2022-06-01/main.js
@@ -0,0 +1,19 @@
+// Coding challenge 06/01/2022
+
+//Implement a function that reverses a string using iteration and then recursion!
+
+
+function reverseStringIterative(str) {
+  let arr = str.split('');
+  let revArr = [];
+
+  if(str.length < 1) return str;
+  
+  while(arr.length > 0) {
+    revArr.push(arr.pop());
+  }
+  return revArr.join('');
+}
+
+console.log(reverseStringIterative('yoyo mastery'));
+//should return: 'yretsam oyoy'

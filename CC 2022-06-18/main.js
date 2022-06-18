ba1daed2f90742a4f0f7323ea525d8293e83a7bd
David Oquendo <davidoqu23@gmail.com>
2022-06-18 16:41:58 -0500

    Solved coding challenge for 06/18/2022

--- CC 2022-06-18/main.js
+++ CC 2022-06-18/main.js
@@ -12,3 +12,48 @@
 // Input: nums = [2,0,1]
 // Output: [0,1,2]
 
+
+// are the elements within the array validated? -> zero, neg, null, undefined, etc..
+// does in-place mean no array manipulation? -> modify nums in place
+// is this basically asking if I know how the sort function works?
+// will the parameter ever NOT contain either 0, 1 or 2? -> always contains 0,1,2
+
+
+// gameplan:
+// nums = [2,0,1] -> count the number of 0, 1 and 2's respectively
+// modify nums -> nums = [0, 1, 2] by re-initializing it based on count in correct order
+
+
+const sortColors = (nums) => {
+  let redCount = 0;
+  let whiteCount = 0;
+  let blueCount = 0;
+
+  // iterate through nums and count 0,1,2
+  for(let i = 0; i < nums.length; i++) {
+      if(nums[i] === 0) {
+          redCount++;
+      } else if(nums[i] === 1) {
+          whiteCount++;
+      } else blueCount++;
+  }
+  
+  // iterate through nums and overwrite based on counters
+  for(let i = 0; i < nums.length; i++) {
+    if(redCount > 0) {
+      nums[i] = 0;
+      redCount--;
+    }else if(whiteCount > 0) {
+      nums[i] = 1;
+      whiteCount--;
+    }else {
+      nums[i] = 2;
+      blueCount--;
+    }
+  }
+};
+
+// time complexity: O(n + n) -> O(2n) iterating twice through same array but not at same time
+// can use two pointers instead of two loops instead
+
+// space complexity: O(1)??

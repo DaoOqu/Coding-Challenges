0b748ec1d0caeae0144c1046f5ef54782c5d7be4
David Oquendo <davidoqu23@gmail.com>
2022-06-19 00:22:08 -0500

    Solved coding challenge for 06/19/2022

--- CC 2022-06-19/main.js
+++ CC 2022-06-19/main.js
@@ -21,3 +21,46 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 
 
 
+// will the string contain non-alphanumeric characters? (meaning anything thats not letters & numbers)
+// will the input ever be empty?
+// will there be any non printable characters? 
+
+// remove non letter and non number characters and spaces
+// change everything that remains to lower case
+// two pointers - one at the beginning one at the end
+// compare as they iterate to the middle (first < last)
+
+const isPalindrome = (s) => {
+  if(s.length < 2) return true;
+  
+  let str = formatString(s);
+  
+  let first = 0;
+  let last = str.length - 1;
+  
+  while(first < last) {
+    if(str[first] !== str[last]) return false;
+    
+    first++;
+    last--;
+  }
+  return true;
+  
+};
+
+const formatString = (s) => {
+  let res = "";
+  
+  for(let char of s) {
+    if(char === " ") continue;
+    
+    let lower = char.toLowerCase();
+    let upper = char.toUpperCase();
+    
+    if(!isNaN(char) || lower !== upper) res += lower;
+  }
+  return res;
+}
+
+// Time complexity: O(n)
+// Space  complexity: O(1)
